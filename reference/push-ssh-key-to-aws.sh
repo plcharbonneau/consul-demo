068a665c81db8cded522771902466ef562e96ce2
Robert Peteuil <robert@peteuil.com>
2020-07-15 13:00:59 -0700

    allow username override

--- reference/push-ssh-key-to-aws.sh
+++ reference/push-ssh-key-to-aws.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Push local ssh key to every region in AWS using AWS CLI
 
 # Set aws_keypair_name to the EC2 key-name in each AWS Region
 #   it must be unique in each region within your account
-aws_keypair_name="$USER"  # or some name that is meaningful to you
+aws_keypair_name="${1:-$USER}"  # use 1st param or current user
 
 # path to PUBLIC ssh key that you want pushed to AWS
 publickeyfile="$HOME/.ssh/id_rsa.pub"
